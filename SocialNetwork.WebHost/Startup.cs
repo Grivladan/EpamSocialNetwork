d40e09af408ff1cecf4c68db906532dfae41bc49
Vladyslav Hryhorenko <grigorenkovlad1993@gmail.com>
2016-09-05 15:18:04 +0300

    create basic project infrastructure

---
+++ SocialNetwork.WebHost/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SocialNetwork.WebHost.Startup))]
+namespace SocialNetwork.WebHost
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
