7a106ebe4d5dc58c0479b58692d6460d37244c25
Vladyslav Hryhorenko <grigorenkovlad1993@gmail.com>
2016-10-02 02:42:12 +0300

    fix menu layout

---
+++ SocialNetwork.WebHost/Controllers/FriendRequestController.cs
@@ -0,0 +1,44 @@
+﻿using SocialNetwork.DataAccess.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using SocialNetwork.DataAccess.Entities;
+
+namespace SocialNetwork.WebHost.Controllers
+{
+    public class FriendRequestController : Controller
+    {
+        // GET: Friend
+        ApplicationDbContext context = new ApplicationDbContext();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(int friendUserId)
+        {
+            var request = new FriendRequest()
+            {
+                RequestedFrom = User.Identity.GetUserId<int>(),
+                RequestedTo = friendUserId,
+                Date = DateTime.Now
+            };
+            context.Requests.Add(request);
+            context.SaveChanges();
+
+            return null;
+        }
+
+        public ActionResult CheckOnWaitingRequest(int id)
+        {
+            var requests = context.Requests.Where(x => x.RequestedTo == id);
+            return View();
+        }
+        
+    }
+}
