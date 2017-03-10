e941dc9af628adc486f7d7b8150b26f4f91812b7
Grivladan <grigorenkovlad1993@gmail.com>
2017-03-10 11:07:58 +0200

    fix validation autocomplete for profile

--- SocialNetwork.DataAccess/Entities/Profile.cs
+++ SocialNetwork.DataAccess/Entities/Profile.cs
@@ -11,30 +11,17 @@ namespace SocialNetwork.DataAccess.Entities
         [ForeignKey("ApplicationUser")]
         public int Id { get; set; }
 
-        [Required(ErrorMessage="First name required")]
+        [Required]
         public string FirstName { get; set; }
 
-        [Required(ErrorMessage = "First name required")]
+        [Required]
         public string LastName { get; set; } 
-
-        [DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString = "{0:dd'/'MM'/'yyyy}", ApplyFormatInEditMode = true)]
-        [Display(Name = "Birthdate")]
         public DateTime? BirthDate { get; set; }
-
-        [RegularExpression(@"([a-zA-Z]+|[a-zA-Z]+\\s[a-zA-Z]+)", ErrorMessage = "Entered country name is not valid.")]
         public string Country { get; set; }
-
-        [RegularExpression(@"([a-zA-Z]+|[a-zA-Z]+\\s[a-zA-Z]+)", ErrorMessage = "Entered city name is not valid.")]
         public string City { get; set; }
-
         public Gender? Gender { get; set; }
-
-        [DataType(DataType.PhoneNumber)]
-        [RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$", ErrorMessage = "Entered phone format is not valid.")]
         public string Phone { get; set; }
         public byte[] UserPhoto { get; set; }
-
         public virtual ApplicationUser ApplicationUser { get; set; }
     }
 }
